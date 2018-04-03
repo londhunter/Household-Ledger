513dbce869109a934d7a31954636558abdd1843c
남장현 <londhunter@gmail.com>
2018-04-04 02:24:47 +0900

    공통코드 화면 작업

--- Household Ledger/Form1.Designer.cs
+++ Household Ledger/Form1.Designer.cs
@@ -33,63 +33,139 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(12, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(253, 304);
+            this.listBox1.Size = new System.Drawing.Size(516, 232);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(297, 252);
+            this.button1.Location = new System.Drawing.Point(181, 258);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "신규입력";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(400, 252);
+            this.button2.Location = new System.Drawing.Point(181, 287);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "저장";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 66);
+            this.label1.Location = new System.Drawing.Point(12, 244);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(16, 12);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(359, 66);
+            this.textBox1.Location = new System.Drawing.Point(75, 260);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "그룹코드";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "코드";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "한글명칭";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(75, 314);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "닫기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 405);
+            this.ClientSize = new System.Drawing.Size(539, 405);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "공통코드";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,6 +179,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Household Ledger/Form1.cs
+++ Household Ledger/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using Household_Ledger.Code_Cls;
 
 namespace Household_Ledger
 {
@@ -24,22 +25,111 @@ namespace Household_Ledger
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string sql = "Select * FROM COMMON_CODE";
-            DataSet ds = new DataSet();
-            OleDbCommand cmd = new OleDbCommand(sql, oleDb);
+            list_setting();
+            button1_Click(sender, e);
+        }
 
-            cmd.CommandText = sql;
+        public void list_setting()
+        {
+            string sql = "SELECT * FROM COMMON_CODE WHERE USE_YN = 'Y' ORDER BY COMM_GRP ASC, COMM_CD ASC";
+            DataSet ds = DataBaseHelper.OleExecuteReader(sql, oleDb);
 
-            OleDbDataReader reader = cmd.ExecuteReader();
-            OleDbDataAdapter oleAdap = new OleDbDataAdapter(sql, oleDb);
+            IList<Common_code> comm_codes = (from row in ds.Tables[0].AsEnumerable()
+                                             select new Common_code
+                                             {
+                                                 Id = row.Field<int>("id"),
+                                                 Comm_grp = row.Field<string>("comm_grp"),
+                                                 Comm_cd = row.Field<string>("comm_cd"),
+                                                 Comm_nm = row.Field<string>("comm_nm")
+                                             }).ToList<Common_code>();
 
-            oleAdap.Fill(ds);
+            listBox1.DisplayMember = "COMM_DISPLAY";
+            listBox1.ValueMember = "ID";
+            listBox1.DataSource = comm_codes;
+        }
 
-            listBox1.Items.Clear();
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Common_code selCcode = (Common_code)listBox1.SelectedItem;
 
-            listBox1.DisplayMember = "COMM_NM";
-            listBox1.ValueMember = "COMM_CD";
-            listBox1.DataSource = ds.Tables[0];
+            label2.Text = selCcode.Id.ToString();
+            textBox1.Text = selCcode.Comm_grp.ToString();
+            textBox2.Text = selCcode.Comm_cd.ToString();
+            textBox3.Text = selCcode.Comm_nm.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string uSql = string.Format("UPDATE COMMON_CODE SET COMM_GRP = '{1}', COMM_CD = '{2}', COMM_NM = '{3}', UPDATE_DT = '{4}' WHERE ID = {0}",
+                label2.Text, textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString("yyyy-MM-dd"));
+
+            string iSql = string.Format("INSERT INTO COMMON_CODE (COMM_GRP, COMM_CD, COMM_NM, USE_YN, INSERT_DT) " +
+                "VALUES ('{0}', '{1}', '{2}', 'Y', '{3}')",
+                textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString("yyyy-MM-dd"));
+            int result = 0;
+
+            if (label2.Text == "")
+            {
+                if (MessageBox.Show("새로운 코드를 등록 하시겠습니까 ?", "코드등록", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    result = DataBaseHelper.OleExecuteNonQuery(iSql, oleDb);
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("해동 코드를 수정 하시겠습니까 ?", "코드등록", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    result = DataBaseHelper.OleExecuteNonQuery(uSql, oleDb);
+                }
+            }
+
+            list_setting();
+
+            if (result == 1)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    class Common_code
+    {
+        public Common_code()
+        {
+        }
+
+        public Common_code(int id, string comm_grp, string comm_cd, string comm_nm)
+        {
+            Id = id;
+            Comm_grp = comm_grp;
+            Comm_cd = comm_cd;
+            Comm_nm = comm_nm;
+        }
+
+        public int Id { get; set; }
+        public string Comm_grp { get; set; }
+        public string Comm_cd { get; set; }
+        public string Comm_nm { get; set; }
+
+        public string comm_display
+        {
+            get
+            {
+                return string.Format("Group : {0} | Code : {1} | Name : {2}", (object)this.Comm_grp, (object)this.Comm_cd, (object)this.Comm_nm);
+            }
         }
     }
 }
